1e2b9845f559e6728eb4ee39c4b3fa0bbc60bb08
Matsua13 <tifeneleblanc@hotmail.fr>
2024-12-08 14:50:23 +0800

    Welcome page ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    flash[:warning] = "This is a warning message" # Set your flash message here
+    # Any other logic you need for the home page
   end
 end
